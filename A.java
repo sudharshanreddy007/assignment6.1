36ef00fe02a22ace90b999e4a4e5c877740478b7
sudharshanreddy007 <31057445+sudharshanreddy007@users.noreply.github.com>
2017-10-30 15:26:14 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,24 @@
+public class A{//only one class can be public in a file with multiple classes
+               //classes are the basics of oops(object oriented programming language)
+public static void main(String[] args){
+System.out.println("This is class A");
+
+}
+}
+//public class B{=> not possible as this is to be declared into separate file B.java
+class B{//created class as B
+public static void main(String[] args){
+              
+System.out.println("This is class B");
+}
+}
+class C{//created class as C
+public static void main(String[] args){
+System.out.println("This is class C");
+}
+}
+class D{//created class as D
+public static void main(String[] args){
+System.out.println("This is class D");
+}
+}
